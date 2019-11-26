c4acd47abd6dfca6830b5c68deb82ac1df9ff077
vdecaro <valeriodc95@hotmail.it>
2019-11-26 02:19:08 +0100

    celery fix

--- celery_entry.py
+++ celery_entry.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
-from flask import current_app as app
-import requests
-from requests.exceptions import Timeout
-from stats import create_app, create_celery
-from stats.models import Stats
 from stats.extensions import celery
+from stats.tasks import poll_inconsistent, poll_refresh
 
 POLLING_RATE = 2.0
 
@@ -16,65 +12,5 @@ def setup_periodic_tasks(sender, **kwargs):
 
     #gather from story microservice
     sender.add_periodic_task(2000*POLLING_RATE, poll_refresh.s(), name='story-microservice')
-
-
-@celery.task
-def poll_inconsistent():
-    try:
-        response = requests.get(f'{app.config["STORIES_ENDPOINT"]}/stories/stats')
-    except Timeout:
-        return
-    
-    to_update = response.json()
-    for author in to_update:
-        auth_stats = Stats.query.get(int(author))
-        add = auth_stats is None
-        if auth_stats is None:
-            auth_stats = Stats()
-        
-        for story in to_update[author]:
-            if story['dice'] != -1:
-                auth_stats.n_dice += story['dice']
-                auth_stats.stories_written += 1
-
-            auth_stats.likes += story['likes']
-            auth_stats.dislikes += story['dislikes']
-
-        if add:
-            db.session.add(auth_stats)
-
-    db.session.commit()
-
-
-@celery.task
-def poll_refresh():
-    try:
-        response = requests.get(f'{app.config["STORIES_ENDPOINT"]}/stories/stats/refresh')
-    except Timeout:
-        return
-    
-    to_update = response.json()
-    for author in to_update:
-        auth_stats = Stats.query.get(int(author))
-
-        add = auth_stats is None
-        if auth_stats is None:
-            auth_stats = Stats()
-
-        auth_stats.n_dice = 0
-        auth_stats.stories_written = 0
-        auth_stats.likes = 0
-        auth_stats.dislikes = 0
-
-        for story in to_update[author]:
-            auth_stats.n_dice += story['dice']
-            auth_stats.stories_written += 1
-            auth_stats.likes += story['likes']
-            auth_stats.dislikes += story['dislikes']
-
-        if add:
-            db.session.add(auth_stats)
-
-    db.session.commit()  
 
 celery.start()
